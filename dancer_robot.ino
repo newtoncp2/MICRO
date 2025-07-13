1d0bb3adf290579920badabc0ab6c15d1ce04a99
newtoncp2 <jeanmicheljoliveira@gmail.com>
2025-07-12 21:48:10 -0300

    funfa pls

--- dancer_robot.ino
+++ dancer_robot.ino
@@ -1,7 +1,6 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
-
 #define SERVO_MIN 150  // Pulso mínimo (~0 graus)
 #define SERVO_MAX 600  // Pulso máximo (~180 graus)
 #define ANGLE_MAX 160
